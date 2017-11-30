da8d66754dbad68bf1d747403c6ed0305639bbe7
Jared <jaredjkim@gmail.com>
2017-11-30 05:50:34 -0800

    first commit

---
+++ server.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, redirect, session ,flash, request
+# import the Connector function
+from mysqlconnection import MySQLConnector
+app = Flask(__name__)
+# connect and store the connection in "mysql" note that you pass the database name to the function
+mysql = MySQLConnector(app, 'friendsdb')
+# @app.route('/')
+# def index():
+#     friends = mysql.query_db("SELECT * FROM friends")
+#     print friends
+#     return render_template('index.html')
+@app.route('/')
+def index():
+    query = "SELECT * FROM friends"                           # define your query
+    friends = mysql.query_db(query)                           # run query with query_db()
+    return render_template('index.html', all_friends=friends) 
+
+@app.route('/friends', methods=['POST'])
+def create():
+    # Write query as a string. Notice how we have multiple values
+    # we want to insert into our query.
+    query = "INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at) VALUES (:first_name, :last_name, :occupation, NOW(), NOW())"
+    # We'll then create a dictionary of data from the POST data received.
+    data = {
+             'first_name': request.form['first_name'],
+             'last_name':  request.form['last_name'],
+             'occupation': request.form['occupation']
+           }
+    # Run query, with dictionary values injected into the query.
+    mysql.query_db(query, data)
+    return redirect('/')
+@app.route('/friends/<friend_id>')
+def show(friend_id):
+    # Write query to select specific user by id. At every point where
+    # we want to insert data, we write ":" and variable name.
+    query = "SELECT * FROM friends WHERE id = :specific_id"
+    # Then define a dictionary with key that matches :variable_name in query.
+    data = {'specific_id': friend_id}
+    # Run query with inserted data.
+    friends = mysql.query_db(query, data)
+    # Friends should be a list with a single object,
+    # so we pass the value at [0] to our template under alias one_friend.
+    return render_template('index.html', one_friend=friends[0])
+app.run(debug=True)
